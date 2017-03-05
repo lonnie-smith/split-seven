de8e2456a80e3f6edb93f30a7d9d795464022093
Lawrence Smith <lawrence.smith.3@gmail.com>
2017-03-05 17:39:43 -0600

    Style up setList page

--- client/src/assets/scripts/audio-player.js
+++ client/src/assets/scripts/audio-player.js
@@ -9,14 +9,42 @@ class Track {
         this.audio.addEventListener('ended', evt => {
             parent.playTrack(trackSeq + 1);
         });
+        this.parent = parent;
+        this.trackSeq = trackSeq;
+        this.$el = $el;
+        this.$el.click(() => { this.toggle(); });
+    }
+
+    toggle() {
+        if (this.playing) {
+            this.pause();
+        } else {
+            this.parent.playTrack(this.trackSeq);
+        }
     }
 
     play() {
         this.audio.play();
+        this.$el.addClass('playing');
+        this.$el.removeClass('paused');
+        this.playing = true;
+        this.paused = false;
     }
 
     pause() {
         this.audio.pause();
+        this.$el.removeClass('playing');
+        this.$el.addClass('paused');
+        this.playing = false;
+        this.paused = true;
+    }
+
+    stop() {
+        this.audio.pause();
+        this.audio.currentTime = 0;
+        this.$el.removeClass('playing paused');
+        this.playing = false;
+        this.paused = false;
     }
 }
 
@@ -27,13 +55,22 @@ class TrackList {
             return new Track(
                 $(el).data('audioFile'), $(el), this, idx);
         });
+        this.tracks[0].pause();
     }
 
     playTrack(trackSeq) {
         if (trackSeq < 0 || trackSeq >= this.tracks.length) {
             return;
         }
-        this.tracks[trackSeq].play();
+        for (const track of this.tracks) {
+            if (track === this.tracks[trackSeq]) {
+                this.tracks[trackSeq].play();
+            } else {
+                if (track.playing || track.paused) {
+                    track.stop();
+                }
+            }
+        }
     }
 }
 
