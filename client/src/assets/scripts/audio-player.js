de22c6a8a931a7b2c8989bb887a324433883892e
Lawrence Smith <lawrence.smith.3@gmail.com>
2017-03-05 12:03:16 -0600

    Add play-through to front-end

--- client/src/assets/scripts/audio-player.js
+++ client/src/assets/scripts/audio-player.js
@@ -1,19 +1,39 @@
 const $ = require('jquery');
 
 class Track {
-    constructor(trackData) {
-        // { filename, filetype, originalname, path, size }
+    constructor(trackData, $el, parent, trackSeq) {
+        // { sequence, artist, title }
         Object.assign(this, trackData);
-        console.debug(trackData)
+        this.audio = $el.find('audio').get(0);
+        this.$img = $el.find('img').first();
+        this.audio.addEventListener('ended', evt => {
+            parent.playTrack(trackSeq + 1);
+        });
+    }
+
+    play() {
+        this.audio.play();
+    }
+
+    pause() {
+        this.audio.pause();
     }
 }
 
 class TrackList {
     constructor() {
         this.tracks = $('[data-audio-file]').get()
-        .map(el => {
-            return new Track($(el).data('audioFile'));
+        .map((el, idx) => {
+            return new Track(
+                $(el).data('audioFile'), $(el), this, idx);
         });
+    }
+
+    playTrack(trackSeq) {
+        if (trackSeq < 0 || trackSeq >= this.tracks.length) {
+            return;
+        }
+        this.tracks[trackSeq].play();
     }
 }
 
